e8bf5e824c6b8a037a24289b284394bd457aa2ea
Yotko Kanchev <yotko.kanchev@gmail.com>
2021-02-20 23:12:18 +0200

    Add Contacts Component

--- mart-max/src/components/shared/footer.js
+++ mart-max/src/components/shared/footer.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+    const date = year > 2021 ? `2021 - ${year}` : 2021
     return (
-
         <div className="footer py-4">
             <div className="container-fluid text-center">
-                <p>All rights Reserved 2021-<span>
-                    <script>document.write(new Date().getFullYear())</script>
-                </span> &copy; MAXMART
+                <p>All rights Reserved {date} &copy; MAXMART
             {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
                     <span className="pl-2">|</span> This template is made with
             <i className="icon-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com"
