cb91e74df9311d75285132816d69fa642fc0457f
Yotko Kanchev <yotko.kanchev@gmail.com>
2021-03-16 11:53:37 +0200

    Add SEO meta information on index.html Set localStorage one week expiration interval.

--- mart-max/src/firebase.js
+++ mart-max/src/firebase.js
@@ -20,23 +20,25 @@ export const fetchData = (collection, setFunction, child) => {
     let data = localStorage.getItem('firebase_data');
 
     if (!data) {
-        console.log('Read from Db')
-        fireDb.on('value', snapshot => {
-            const values = snapshot.val();
-            localStorage.setItem('firebase_data', JSON.stringify(values));
-
-            if (!child) {
-                setFunction(values[collection])
-            } else {
-                setFunction(values[collection][child]);
-            }
-        })
+        readFromDb(collection, setFunction, child);
     } else {
-        console.log("Read from localStorage")
-        if (!child) {
-            setFunction(JSON.parse(data)[collection])
+        const setDate = JSON.parse(localStorage.getItem('setDate'));
+
+        if (setDate) {
+            const currentDate = new Date();
+            const diffTime = Math.abs(currentDate.getTime() - setDate);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+            // clear localStorage and read data from DB
+            if (diffDays >= 7) {
+                localStorage.clear();
+                readFromDb(collection, setFunction, child);
+            } else {
+                readFromLocalStorage(data, collection, setFunction, child);
+            }
         } else {
-            setFunction(JSON.parse(data)[collection][child]);
+            localStorage.clear();
+            readFromDb(collection, setFunction, child);
         }
     }
 }
@@ -65,4 +67,29 @@ export const appAuth = firebase.auth();
 export const signOut = () => {
     firebase.auth().signOut();
     localStorage.clear();
+}
+
+const readFromDb = (collection, setFunction, child) => {
+    console.log('Read from Db')
+    fireDb.on('value', snapshot => {
+        const values = snapshot.val();
+        localStorage.setItem('firebase_data', JSON.stringify(values));
+        const now = new Date();
+        localStorage.setItem('setDate', JSON.stringify(now.getTime()));
+
+        if (!child) {
+            setFunction(values[collection])
+        } else {
+            setFunction(values[collection][child]);
+        }
+    })
+}
+
+const readFromLocalStorage = (data, collection, setFunction, child) => {
+    console.log("Read from localStorage")
+    if (!child) {
+        setFunction(JSON.parse(data)[collection])
+    } else {
+        setFunction(JSON.parse(data)[collection][child]);
+    }
 }
